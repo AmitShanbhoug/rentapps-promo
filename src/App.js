21f172bd7715438ed04484297f586fb79a57d9c1
Patrick <phuan097@uottawa.ca>
2021-01-07 20:53:29 -0500

    Changed tab title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Features from './Features';
@@ -6,6 +6,9 @@ import Beta from './Beta';
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    document.title = "RentApps"
+  }, []);
   return (
     <div>
       <BrowserRouter>
